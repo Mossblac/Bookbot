b36c86232bcc13e01240243075f286577484cdd5
Mossblac <Murphyrmurphy2000@gmail.com>
2025-02-07 10:26:57 -0700

    main counts characters to dictionary

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 def main():
     with open("books/Frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+        lower_text = file_contents.lower()
+
+    character_count = {}
+    
+
+    
+            
+    
+    
+    for char in lower_text:
+        character_count[char] = character_count.get(char, 0) + 1
+        
+       
+            
+
+    print(character_count)
+
+
+
+    
     
 
 main()
